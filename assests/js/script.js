bc0424b7d8d619b163d4168c9a4c0efad142cc20
Brechin Andariese <brechin@brechy.net>
2024-11-22 13:41:04 -0600

    Add javascript functionality for hamburger menu.

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,21 @@
+//Nav hamburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+})
+
+//Close hamburger menu when a menu link is clicked
+const navLink = document.querySelector(".nav-link");
+
+navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+})
+
 // scroll to top functionality
 const scrollUp = document.querySelector("#scroll-up");
 
